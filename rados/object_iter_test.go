bb34d4005cbbf37e92e0ecb07709c8a820a54410
John Mulligan <jmulligan@redhat.com>
2020-01-02 20:46:40 -0500

    rados: add test cases for new ObjectsIter type
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- rados/object_iter_test.go
+++ rados/object_iter_test.go
@@ -1,6 +1,7 @@
 package rados
 
 import (
+	"fmt"
 	"sort"
 
 	"github.com/stretchr/testify/assert"
@@ -141,4 +142,128 @@ func (suite *RadosTestSuite) TestObjectIteratorAcrossNamespaces() {
 	sort.Strings(rogueList)
 	sort.Strings(existingList)
 	assert.Equal(suite.T(), rogueList, existingList)
+}
+
+func (suite *RadosTestSuite) TestObjectsIterList() {
+	suite.SetupConnection()
+
+	initialObjectNames := []string{}
+	suite.ioctx.SetNamespace(RadosAllNamespaces)
+	oiter, err := NewObjectsIter(suite.ioctx)
+	assert.NoError(suite.T(), err)
+
+	for {
+		entry, err := oiter.Next()
+		if err == RadosErrorNotFound {
+			break
+		}
+		if assert.NoError(suite.T(), err) {
+			initialObjectNames = append(initialObjectNames, entry.Entry)
+		}
+	}
+	oiter.Close()
+
+	suite.ioctx.SetNamespace("test-ns-1")
+	created := []string{}
+	for i := 0; i < 10; i++ {
+		oid := writeDummyObject(
+			suite, suite.ioctx, fmt.Sprintf("input data %d", i))
+		created = append(created, oid)
+	}
+	defer cleanObjects(suite, "test-ns-1", created)
+
+	ns1ObjectNames := []string{}
+	oiter, err = NewObjectsIter(suite.ioctx)
+	assert.NoError(suite.T(), err)
+	for {
+		entry, err := oiter.Next()
+		if err == RadosErrorNotFound {
+			break
+		}
+		if assert.NoError(suite.T(), err) {
+			ns1ObjectNames = append(ns1ObjectNames, entry.Entry)
+		}
+	}
+	oiter.Close()
+
+	sort.Strings(created)
+	sort.Strings(ns1ObjectNames)
+	assert.Equal(suite.T(), created, ns1ObjectNames)
+
+	expected := []string{}
+	expected = append(expected, initialObjectNames...)
+	expected = append(expected, ns1ObjectNames...)
+
+	suite.ioctx.SetNamespace("test-ns-2")
+	oid := writeDummyObject(suite, suite.ioctx, "more data")
+	defer cleanObjects(suite, "test-ns-2", []string{oid})
+	expected = append(expected, oid)
+
+	currObjectNames := []string{}
+	foundNs := map[string]int{}
+	suite.ioctx.SetNamespace(RadosAllNamespaces)
+	oiter, err = NewObjectsIter(suite.ioctx)
+	assert.NoError(suite.T(), err)
+	for {
+		entry, err := oiter.Next()
+		if err == RadosErrorNotFound {
+			break
+		}
+		if assert.NoError(suite.T(), err) {
+			currObjectNames = append(currObjectNames, entry.Entry)
+			foundNs[entry.Namespace] += 1
+		}
+	}
+	oiter.Close()
+
+	sort.Strings(expected)
+	sort.Strings(currObjectNames)
+	assert.Equal(suite.T(), expected, currObjectNames)
+	assert.Equal(suite.T(), 10, foundNs["test-ns-1"])
+	assert.Equal(suite.T(), 1, foundNs["test-ns-2"])
+}
+
+func (suite *RadosTestSuite) TestObjectsIterSeekToken() {
+	suite.SetupConnection()
+
+	suite.ioctx.SetNamespace("test-ns-1")
+	created := []string{}
+	for i := 0; i < 10; i++ {
+		oid := writeDummyObject(
+			suite, suite.ioctx, fmt.Sprintf("input data %d", i))
+		created = append(created, oid)
+	}
+	defer cleanObjects(suite, "test-ns-1", created)
+
+	oiter, err := NewObjectsIter(suite.ioctx)
+	assert.NoError(suite.T(), err)
+
+	token1 := oiter.Token()
+	names1 := []string{}
+	for {
+		entry, err := oiter.Next()
+		if err == RadosErrorNotFound {
+			break
+		}
+		if assert.NoError(suite.T(), err) {
+			names1 = append(names1, entry.Entry)
+		}
+	}
+
+	oiter.Seek(token1)
+	names2 := []string{}
+	for {
+		entry, err := oiter.Next()
+		if err == RadosErrorNotFound {
+			break
+		}
+		if assert.NoError(suite.T(), err) {
+			names2 = append(names2, entry.Entry)
+		}
+	}
+	oiter.Close()
+
+	assert.Equal(suite.T(), 10, len(names1))
+	assert.Equal(suite.T(), 10, len(names2))
+	assert.Equal(suite.T(), names1, names2)
 }
