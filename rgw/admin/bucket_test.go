186c4bbf12227cf316df54647877a9f8b3da52a2
John Mulligan <jmulligan@redhat.com>
2021-05-24 14:19:19 -0400

    xxx: rgw/admin/bucket_test.go: xxx:hax
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- rgw/admin/bucket_test.go
+++ rgw/admin/bucket_test.go
@@ -2,15 +2,49 @@ package admin
 
 import (
 	"context"
+	"log"
+	"net/http"
+	"net/http/httputil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+type traceClient struct {
+	HTTPClient HTTPClient
+}
+
+func (t *traceClient) Do(request *http.Request) (*http.Response, error) {
+	dump, err := httputil.DumpRequestOut(request, true)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("\n--- request ---\n%s\n", string(dump))
+
+	// Send HTTP request
+	resp, err := t.HTTPClient.Do(request)
+	if err != nil {
+		log.Printf("\n--- error ---\n%s\n", err)
+		return nil, err
+	}
+
+	dump, err = httputil.DumpResponse(resp, true)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("\n--- response ---\n%s\n", string(dump))
+
+	return resp, nil
+}
+
+func debugClient(c HTTPClient) HTTPClient {
+	return &traceClient{c}
+}
+
 func (suite *RadosGWTestSuite) TestBucket() {
 	suite.SetupConnection()
 	co, err := New(suite.endpoint, suite.accessKey, suite.secretKey, nil)
-	co.Debug = true
+	co.HTTPClient = debugClient(co.HTTPClient)
 	assert.NoError(suite.T(), err)
 
 	suite.T().Run("list buckets", func(t *testing.T) {
